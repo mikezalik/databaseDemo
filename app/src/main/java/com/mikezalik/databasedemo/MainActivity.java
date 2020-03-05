f515bfafba6bb9f500a8b48e3e52779aa58795ae
mikezalik <mike.zalik@gmail.com>
2020-03-05 09:29:21 -0800

    Adds name/age insert and cursor move

--- app/src/main/java/com/mikezalik/databasedemo/MainActivity.java
+++ app/src/main/java/com/mikezalik/databasedemo/MainActivity.java
@@ -2,8 +2,10 @@ package com.mikezalik.databasedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
-        myDatabase.execSQL("CREATE TABLE IF NOT EXIST users (name VARCHAR, age INT(3))");
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+
+        myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Mike', 29)");
+        myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Brianna', 24)");
+
+        Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+
+        int nameIndex = c.getColumnIndex("name");
+        int ageIndex = c.getColumnIndex("age");
+
+        c.moveToFirst();
+
+        while (!c.isAfterLast()){
+            Log.i("name", c.getString(nameIndex));
+            Log.i("age", c.getString(ageIndex));
+
+            c.moveToNext();
+        }
     }
 }
